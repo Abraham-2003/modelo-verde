e5638ca982f2b8f2a7706d2fb457d258f3882214
Estudiantes-de-la-Bibiblia <abrahamren03@gmail.com>
2025-08-03 18:40:26 -0600

    ajuste en el servidor

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/modelo-verde', express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
